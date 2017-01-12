d28480e62f69f793cc8364e5a23c7f5031f274ff
RichardLentz <triplelstudiomail@gmail.com>
2017-01-11 21:10:24 -0500

    first commit

---
+++ testfunc.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+def testfunc():
+    c = np.array([1,2,3,4])
+    y = np.array([5,6,7,8])
+    print(c + y)
+    
+testfunc()
+
---
+++ testfunc2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 11 09:42:32 2017
+
+@author: richard
+"""
+
+import numpy as np
+
+def testfunc2():
+    a = np.genfromtxt('data001.csv', delimiter = ',')
+    print(a)
+    
+testfunc2()
+
+
+    
